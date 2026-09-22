06cf743a758823a4efeaede9965a9545f840d4b4
Sandra Tomàs <sandratv14.7@gmail.com>
2026-09-22 16:08:08 +0200

    Creation of a script similar to plot_goodness.py but for the full fitting where all the parameters are free. In here we don't have ensambles. Also the corresponding plots generated.

--- P-value/plot_goodness_full.py
+++ P-value/plot_goodness_full.py
@@ -18,32 +18,28 @@ mpl.rcParams['mathtext.fontset'] = 'stix'  # Matches the math rendering to the t
 A_types = ['Ar0', 'Api12']
 A_latex = ['r_0','\pi/12']
 # Change index to select r0 or pi12 data
-A_index = 1
+A_index = 0
 # Global?
 glob = False
 
 if glob == True:
     # Load the fit results
-    df_lin = pd.read_csv(f'fit_results_linear_global_MC_{A_types[A_index]}.csv')
-    df_log = pd.read_csv(f'fit_results_log_global_{A_types[A_index]}.csv')
-    df_ST = pd.read_csv(f'fit_results_ST_global_{A_types[A_index]}.csv')
+    df_lin = pd.read_csv(f'All fitted/fit_results_full_global_linear_{A_types[A_index]}.csv')
+    df_log = pd.read_csv(f'All fitted/fit_results_full_global_logarithmic_{A_types[A_index]}.csv')
+    df_ST = pd.read_csv(f'All fitted/All fitted ST/fit_results_full_ST_global_{A_types[A_index]}.csv')
     #When global fit is performed
-    pivot_lin = df_lin.pivot(index='Data_Type', columns='Branch', values='p_value')
-    pivot_log = df_log.pivot(index='Data_Type', columns='Branch', values='p_value')
+    pivot_lin = df_lin.set_index('Data_Type')[['p_value']]
+    pivot_log = df_log.set_index('Data_Type')[['p_value']]
     pivot_ST = df_ST.set_index('Data_Type')[['p_value']]
 if glob == False:
     # Load the fit results
-    df_lin = pd.read_csv(f'fit_results_linear_MC_{A_types[A_index]}.csv')
-    df_log = pd.read_csv(f'fit_results_log_{A_types[A_index]}.csv')
-    df_ST = pd.read_csv(f'fit_results_ST_{A_types[A_index]}.csv')
+    df_lin = pd.read_csv(f'All fitted/fit_results_full_linear_{A_types[A_index]}.csv')
+    df_log = pd.read_csv(f'All fitted/fit_results_full_logarithmic_{A_types[A_index]}.csv')
+    df_ST = pd.read_csv(f'All fitted/All fitted ST/fit_results_full_ST_{A_types[A_index]}.csv')
     # Create a combined configuration label (Ensemble + Data Type)
-    #When separated by ensambles
-    df_lin['Config'] = df_lin['Ensemble'] + " (" + df_lin['Data_Type'] + ")"
-    df_log['Config'] = df_log['Ensemble'] + " (" + df_log['Data_Type'] + ")"
-    df_ST['Config'] = df_ST['Ensemble'] + " (" + df_ST['Data_Type'] + ")"
 
-    pivot_lin = df_lin.pivot(index='Config', columns='Branch', values='p_value')
-    pivot_log = df_log.pivot(index='Config', columns='Branch', values='p_value')
+    pivot_lin = df_lin.pivot(index='Ensemble', columns='Data_Type', values='p_value')
+    pivot_log = df_log.pivot(index='Ensemble', columns='Data_Type', values='p_value')
     pivot_ST = df_ST.pivot(index='Ensemble', columns='Data_Type', values='p_value')
 
 # ==========================================
@@ -62,23 +58,23 @@ sns.heatmap(
     vmax=1.5
 )
 if glob == True:
-    ax_lin.set_title(f'Global Linear Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_lin.set_title(f'Full Global Linear Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
     ax_lin.set_ylabel('Data Type', fontsize=14)
-    ax_lin.set_xlabel('Branch', fontsize=12)
+    ax_lin.set_xlabel('p-value', fontsize=12)
 
     fig_lin.tight_layout()
-    fig_lin.savefig(f'P_value_Linear_Global_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_lin.savefig(f'P_value_full_Linear_Global_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_lin)
-    print(f"Saved 'P_value_Linear_Global_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_Linear_Global_{A_types[A_index]}.svg'")
 if glob == False:
-    ax_lin.set_title(f'Linear Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
-    ax_lin.set_ylabel('Ensemble (Data Type)', fontsize=14)
-    ax_lin.set_xlabel('Branch', fontsize=12)
+    ax_lin.set_title(f'Full Linear Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_lin.set_ylabel('Ensemble', fontsize=14)
+    ax_lin.set_xlabel('Data Type', fontsize=12)
 
     fig_lin.tight_layout()
-    fig_lin.savefig(f'P_value_Linear_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_lin.savefig(f'P_value_full_Linear_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_lin)
-    print(f"Saved 'P_value_Linear_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_Linear_{A_types[A_index]}.svg'")
 
 
 # ==========================================
@@ -97,23 +93,23 @@ sns.heatmap(
     vmax=1.5
 )
 if glob == True:
-    ax_log.set_title(f'Global Log Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_log.set_title(f'Full Global Log Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
     ax_log.set_ylabel('Data Type', fontsize=14)
-    ax_log.set_xlabel('Branch', fontsize=12)
+    ax_log.set_xlabel('p-value', fontsize=12)
 
     fig_log.tight_layout()
-    fig_log.savefig(f'P_value_Log_Global_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_log.savefig(f'P_value_full_Log_Global_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_log)
-    print(f"Saved 'P_value_Log_Global_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_Log_Global_{A_types[A_index]}.svg'")
 if glob == False:
-    ax_log.set_title(f'Log Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
-    ax_log.set_ylabel('Ensemble (Data Type)', fontsize=14)
-    ax_log.set_xlabel('Branch', fontsize=12)
+    ax_log.set_title(f'Full Log Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_log.set_ylabel('Ensemble', fontsize=14)
+    ax_log.set_xlabel('Data Type', fontsize=12)
 
     fig_log.tight_layout()
-    fig_log.savefig(f'P_value_Log_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_log.savefig(f'P_value_full_Log_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_log)
-    print(f"Saved 'P_value_Log_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_Log_{A_types[A_index]}.svg'")
 
 # ==========================================
 # 3. Soto-Tarrús model
@@ -131,22 +127,22 @@ sns.heatmap(
     vmax=1.5
 )
 if glob == True:
-    ax_ST.set_title(f'Global ST Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_ST.set_title(f'Full Global ST Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
     ax_ST.set_ylabel('Data Type', fontsize=14)
     ax_ST.set_xlabel('p-value', fontsize=14)
     ax_ST.set_xticklabels(ax_ST.get_xticklabels(), rotation=0)
     ax_ST.set_yticklabels(ax_ST.get_yticklabels(), rotation=0)
 
     fig_ST.tight_layout()
-    fig_ST.savefig(f'P_value_ST_Global_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_ST.savefig(f'P_value_full_ST_Global_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_ST)
-    print(f"Saved 'P_value_ST_Global_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_ST_Global_{A_types[A_index]}.svg'")
 if glob == False:
-    ax_ST.set_title(f'ST Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
+    ax_ST.set_title(f'Full ST Model: p-value ($A=A_{{{A_latex[A_index]}}}$)', fontsize=16)
     ax_ST.set_ylabel('Ensemble', fontsize=14)
     ax_ST.set_xlabel('Data Type', fontsize=14)
 
     fig_ST.tight_layout()
-    fig_ST.savefig(f'P_value_ST_{A_types[A_index]}.svg', bbox_inches='tight')
+    fig_ST.savefig(f'P_value_full_ST_{A_types[A_index]}.svg', bbox_inches='tight')
     plt.close(fig_ST)
-    print(f"Saved 'P_value_ST_{A_types[A_index]}.svg'")
+    print(f"Saved 'P_value_full_ST_{A_types[A_index]}.svg'")
